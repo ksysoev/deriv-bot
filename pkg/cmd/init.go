0ee0a6456adb1268b579c0494581ad86cd1fa746
Kirill Sysoev <k.sysoev@me.com>
2025-06-24 21:20:37 +0800

    Add initial project structure with CLI framework
    - Set up `go.mod` and `go.sum` for dependency management.
    - Implement basic CLI structure using `cobra` with placeholder commands:
      - `run` command with `all` subcommand.
    - Add `main.go` to initialize and execute the CLI application.
    - Update `.gitignore` to include IDE-specific files (`.idea/` and `.vscode/`).

---
+++ pkg/cmd/init.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+type BuildInfo struct {
+	Version string
+}
+
+type cmdArgs struct {
+	LogLevel   string `mapstructure:"log_level"`
+	Version    string
+	TextFormat bool `mapstructure:"log_text"`
+}
+
+func InitCommand(build BuildInfo) cobra.Command {
+	args := &cmdArgs{
+		Version: build.Version,
+	}
+
+	cmd := cobra.Command{
+		Use:   "bot",
+		Short: "Service for running bots for Deriv API",
+		Long:  "Service for running tranding bots and stream trading signal from markets data from Deriv API",
+	}
+
+	cmd.AddCommand(initRunCommand(args))
+
+	return cmd
+}
+
+func initRunCommand(args *cmdArgs) *cobra.Command {
+	runCmd := &cobra.Command{
+		Use:   "run",
+		Short: "Run the bot",
+		Long:  "Run the bot with the specified configuration.",
+	}
+
+	cmdRunAll := &cobra.Command{
+		Use:   "all",
+		Short: "Run all services",
+		Long:  "Run all services",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			return runAllServices(cmd.Context(), args)
+		},
+	}
+
+	runCmd.AddCommand(cmdRunAll)
+
+	return runCmd
+}
---
+++ pkg/cmd/run.go
@@ -0,0 +1,10 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+)
+
+func runAllServices(_ context.Context, _ *cmdArgs) error {
+	return fmt.Errorf("not implemented")
+}
